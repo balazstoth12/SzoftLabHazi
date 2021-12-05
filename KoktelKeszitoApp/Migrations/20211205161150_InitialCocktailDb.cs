116951927e91820bc4b2c765d1fa806bf67f3be4
Balázs Tóth <balazstoth12@edu.bme.hu>
2021-12-05 20:42:57 +0100

    Alap controllerek, a migrációkkal

---
+++ KoktelKeszitoApp/Migrations/20211205161150_InitialCocktailDb.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KoktelKeszitoApp.Migrations
+{
+    public partial class InitialCocktailDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavouriteUser",
+                columns: table => new
+                {
+                    FavouriteUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavouriteUser", x => x.FavouriteUserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantity = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cocktails",
+                columns: table => new
+                {
+                    CocktailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CocktailName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    MadeByUserId = table.Column<int>(type: "int", nullable: true),
+                    MadeDate = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cocktails", x => x.CocktailId);
+                    table.ForeignKey(
+                        name: "FK_Cocktails_Users_MadeByUserId",
+                        column: x => x.MadeByUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserFavouriteUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    FavouriteUserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFavouriteUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserFavouriteUsers_FavouriteUser_FavouriteUserId",
+                        column: x => x.FavouriteUserId,
+                        principalTable: "FavouriteUser",
+                        principalColumn: "FavouriteUserId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserFavouriteUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CocktailIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CocktailId = table.Column<int>(type: "int", nullable: false),
+                    IngredientsId = table.Column<int>(type: "int", nullable: false),
+                    IngredientId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CocktailIngredients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CocktailIngredients_Cocktails_CocktailId",
+                        column: x => x.CocktailId,
+                        principalTable: "Cocktails",
+                        principalColumn: "CocktailId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CocktailIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCocktails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CocktailId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCocktails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserCocktails_Cocktails_CocktailId",
+                        column: x => x.CocktailId,
+                        principalTable: "Cocktails",
+                        principalColumn: "CocktailId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCocktails_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailIngredients_CocktailId",
+                table: "CocktailIngredients",
+                column: "CocktailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CocktailIngredients_IngredientId",
+                table: "CocktailIngredients",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cocktails_MadeByUserId",
+                table: "Cocktails",
+                column: "MadeByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCocktails_CocktailId",
+                table: "UserCocktails",
+                column: "CocktailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCocktails_UserId",
+                table: "UserCocktails",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavouriteUsers_FavouriteUserId",
+                table: "UserFavouriteUsers",
+                column: "FavouriteUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFavouriteUsers_UserId",
+                table: "UserFavouriteUsers",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CocktailIngredients");
+
+            migrationBuilder.DropTable(
+                name: "UserCocktails");
+
+            migrationBuilder.DropTable(
+                name: "UserFavouriteUsers");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Cocktails");
+
+            migrationBuilder.DropTable(
+                name: "FavouriteUser");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
